5ab92b89f5e1621c1c581dd0b93a73a5935e4850
starcargit <edv@starcar.de>
2022-04-13 09:46:04 +0200

    docker

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize';
 import { dbConfig } from '../../config';
+import logger from '../lib/log';
 import User, { users } from './users';
 // import Station, { stations } from './stations';
 
@@ -10,6 +11,17 @@ const { database, username, password, ...options } = config;
 // cfg aus env, sonst leerer string. jedenfalls nicht undefined (siehe config.ts)
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 export const sequelize = new Sequelize(database!, username!, password!, options);
+
+const testSequelizeConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    logger.info('Datenbank verbunden');
+  } catch (error) {
+    logger.error('Datenbank nicht verbunden');
+  }
+};
+
+testSequelizeConnection();
 
 User.init(users, { tableName: 'users', sequelize });
 // Station.init(stations, { tableName: 'stations', sequelize });
---
+++ todo.md
@@ -0,0 +1,14 @@
+init, env und db (+ domain?) verbindung prüfen?
+
+testconnection wird jedes mal abgefragt?
+
+---
+
+dev umgebung:
+
+leere db erstellen etc
+user mich eintragen (siehe db/index?)
+andere testdaten eintragen? (station?)
+
+https://github.com/vercel/next.js/blob/canary/docs/deployment.md#docker-image=
+https://github.com/vercel/next.js/tree/canary/examples/with-docker-multi-env
