28d4706cd9aa2007fbecca2d41fda261f6f76a42
olebergen <ole.bergen.92@gmail.com>
2022-05-25 15:07:23 +0200

    deploy cd

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# working dir -> root vom projekt
+cd "${0%/*}" || exit 1
+
 docker-compose down
 git pull
 docker-compose build --build-arg HTTP_PROXY=http://starcarproxy.pop-i.cloud:8080 --build-arg HTTPS_PROXY=http://starcarproxy.pop-i.cloud:8080
